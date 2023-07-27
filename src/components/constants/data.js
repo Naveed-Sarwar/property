1f0a0365ef672df8439838b62dd664d4649e297a
Naveed Sarwar <naveedsarwar@Naveeds-MacBook-Pro.local>
2023-07-27 11:46:11 -0700

    Wg

--- src/components/constants/data.js
+++ src/components/constants/data.js
@@ -9,10 +9,10 @@ import { SiArtixlinux } from "react-icons/si";
 
 export const list = [
     { title: "Invest", icon: <FiDollarSign fontSize={24} />, route: "https://property-page-blue.vercel.app/" },
-    { title: "Buy Land", icon: <BiSolidLandscape fontSize={24} />, route: "/" },
+    { title: "Buy Land", icon: <BiSolidLandscape fontSize={24} />, route: "https://beta.strixdevelopment.net/Aasthi/real-estate/#" },
     { title: "Investment Analyzer", icon: <SiArtixlinux fontSize={24} />, route: "https://property-rho.vercel.app/page" },
     { title: "Portfolio", icon: <BsFillPersonFill fontSize={24} />, route: "https://profound-flea.10web.site/" },
-    { title: "On ramp a property", icon: <AiFillPropertySafety fontSize={24} />, route: "/" },
-    { title: "Use Your Land", icon: <FaTools fontSize={22} />, route: "/" },
-    { title: "Build on Aasthi", icon: <AiTwotoneTool fontSize={24} />, route: "/" }
+    { title: "On ramp a property", icon: <AiFillPropertySafety fontSize={24} />, route: "http://aasthi.ai" },
+    { title: "Use Your Land", icon: <FaTools fontSize={22} />, route: "/http://aasthi.ai/" },
+    { title: "Build on Aasthi", icon: <AiTwotoneTool fontSize={24} />, route: "http://aasthi.ai" }
 ];
