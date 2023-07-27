13af5355e37dac43ceef5a67c2dd616031d706d9
Naveed Sarwar <naveedsarwar@Naveeds-MacBook-Pro.local>
2023-07-27 07:05:58 -0700

    woking

--- src/components/constants/data.js
+++ src/components/constants/data.js
@@ -3,9 +3,11 @@ import { BiSolidLandscape } from "react-icons/bi";
 import { BsFillPersonFill } from 'react-icons/bs';
 import { AiFillPropertySafety, AiTwotoneTool } from "react-icons/ai";
 import { FaTools } from "react-icons/fa";
+import {FiDollarSign} from "react-icons/fi";
 
 export  const list = [
-    { title: "Keep Invest", icon: <BiSolidLandscape fontSize={24} /> },
+    {title: "Invest" , icon: <FiDollarSign fontSize={24} /> },
+    { title: "Buy Land", icon: <BiSolidLandscape fontSize={24} /> },
     { title: "Portfolio", icon: <BsFillPersonFill fontSize={24} /> },
     { title: "On ramp a property", icon: <AiFillPropertySafety fontSize={24} /> },
     { title: "Use Your Land", icon: <FaTools fontSize={22} /> },
