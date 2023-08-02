0bfc864dca98278d60a3834768e4f07187614a77
Naveed Sarwar <naveedsarwar@Naveeds-MacBook-Pro.local>
2023-08-02 10:31:47 -0700

    working

--- src/components/buyLandCard/BuyLandCard.js
+++ src/components/buyLandCard/BuyLandCard.js
@@ -7,7 +7,7 @@ const BuyLandCard = () => {
      
         <div className='h-auto w-[90%] lg:w-64 lg:h-84 mt-4 rounded-md bg-[#1d222d] border-[#575555] border-[1px] p-4'>
             <Image src={Land} alt='land' />
-            <p className='font-normal text-xl text-[#d1d5db]'>APN 1011020335475100600, Belen, NM</p>
+            <p className='font-normal text-xl py-1 text-[#d1d5db]'> APN 174-783 Hyderabad ,Telangana</p>
             <p className='text-[#9ca3af]'>Valencia, NM</p>
             <div className='bg-[#111827] mt-2 text-center w-28 rounded-3xl p-2'>
                 <span className='text-[#e0e0e0]'>2.3k</span><span className='text-[#9ca3af]'>USDC</span>
